b11e0519e33bd4e3c06115b2b662ce38a3aeca67
Asal MacBook 2 <asal@asals-mbp.mynet>
2020-03-31 14:44:45 +0300

    create TokenUtil class

---
+++ Demo-Project-for-Java-spring/src/main/java/com/example/demo/security/TokenUtil.java
@@ -0,0 +1,36 @@
+package com.example.demo.security;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class TokenUtil {
+    private final String CLAIMS_SUBJECT = "sub";
+    private final String CLAIMS_CREATED = "created";
+    @Value("${auth.expirtaion}")
+    private Long TOKEN_VALIDATIY = 604800L; // 7 Days in seconds
+    @Value("${auth.secret}")
+    private String TOKEN_SECRET = "movieAPISecret";
+
+    public String generateToken(UserDetails userDetils) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(CLAIMS_SUBJECT, userDetils.getUsername());
+        claims.put(CLAIMS_CREATED, new Date());
+
+        return Jwts.builder().setClaims(claims).setExpiration(generateExperiation())
+                .signWith(SignatureAlgorithm.HS512, TOKEN_SECRET).compact();
+
+    }
+
+    private Date generateExperiation() {
+        return new Date(System.currentTimeMillis() + TOKEN_VALIDATIY * 1000);// convert to millisecond
+    }
+}
