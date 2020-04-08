851699388a9e4d234d2d3faa75ea68b5a2cd2bfd
Asal MacBook 2 <asal@asals-mbp.mynet>
2020-04-08 10:27:34 +0300

    create auth,tokenStorage,user service

--- Angular -Front-End/movie-website/src/app/shared/service/be-read-fav-movie.service.ts
+++ Angular -Front-End/movie-website/src/app/shared/service/be-read-fav-movie.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/Rx';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeReadFavMovieService {
-  movieFavServices
+  URLFAVORITEMOVIE = "http:localhost:8080/api/favMovie/"
   constructor(private http:HttpClient) { }
+  getUserFavoriteMovie(userId:string, jwt:string){
+    return this.http.get('$URLFAVORITEMOVIE/${userId}')
+
+  }
 }
