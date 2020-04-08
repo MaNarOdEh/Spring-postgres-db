78247893907f181bea9fad741b9a6cb80b2510f3
Asal MacBook 2 <asal@asals-mbp.mynet>
2020-04-08 12:23:53 +0300

    navigate to login page when user click login icons

--- Angular -Front-End/movie-website/src/app/header/header.component.ts
+++ Angular -Front-End/movie-website/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myrouter:Router) { }
 
   ngOnInit(): void {
   }
   naviagteToMostPopular(){
   }
   naviagteToMostRating(){
+  }
+  showLogin(){
+    this.myrouter.navigate(['/login']);
   }
 
 }
