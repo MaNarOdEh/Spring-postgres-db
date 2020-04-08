25f2d919632beed911618772b0edeb066f59dc19
Asal MacBook 2 <asal@asals-mbp.mynet>
2020-04-08 09:43:26 +0300

    create angular part

---
+++ Angular -Front-End/movie-website/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieCardComponent } from './component/movie-card/movie-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MovieListComponent,
+    MovieCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
