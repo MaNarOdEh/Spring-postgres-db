832b5167a9c2d86236ede37f75386a867c7b40ad
Manar Odeh <manar-_odeh@hotmail.com>
2020-04-14 00:08:13 +0300

    add some commands

--- Angular -Front-End/movie-website/src/app/store/Action/login.action.ts
+++ Angular -Front-End/movie-website/src/app/store/Action/login.action.ts
@@ -1,7 +1,25 @@
-import { createAction, props } from "@ngrx/store";
+import { createAction, props, Action } from "@ngrx/store";
 
 export const login = createAction(
   "[Login Page] Login",
   props<{ username: string; password: string }>()
 );
 export const singout = createAction("[Signout Page] Signout");
+
+/*
+createAction method will create a class for each action
+you can assume that the first action login will be something like that
+
+
+  export class login implements Action{
+    type:string='login';
+    public constructor(public username,public password){
+      
+    }
+  }
+  export class signout implements Action{
+    type:string='signout'
+  }
+
+  
+*/
--- Angular -Front-End/movie-website/src/app/store/Reducer/login.reducer.ts
+++ Angular -Front-End/movie-website/src/app/store/Reducer/login.reducer.ts
@@ -3,12 +3,14 @@ import { login, singout } from "../Action/login.action";
 
 export const isLogin = false;
 
-/*State changes are handled by pure functions (reducer)*/
+/*State changes are handled by pure functions (reducer)
+we create a reducer that it will handle & dealing with each action*/
 const _loginReducer = createReducer(
   isLogin,
   on(login, (state) => (state = true)),
   on(singout, (state) => (state = false))
 );
+
 /*Action is an interface that have anattributes called type*/
 export function reducer(state, action: Action) {
   return _loginReducer(state, action);
